7727e3a1ded5908212e8a424aa97d6a55fa46c9b
Ene AL <kaixo.eneal@gmail.com>
2023-09-29 20:21:10 +0200

    fix pw

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from dotenv import load_dotenv
 from flask_migrate import Migrate
@@ -10,6 +11,7 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+bc = Bcrypt(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
