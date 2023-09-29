5f1200f5aa40673e5537f022edcc81a597ed937b
Ene AL <kaixo.eneal@gmail.com>
2023-09-29 17:24:12 +0200

    fix db model

--- app.py
+++ app.py
@@ -27,12 +27,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String, unique=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
 
     def __init__(self, username, password, email):
         self.username = username
         self.password = password
         self.email = email
+
 
 
 class UserSchema(ma.Schema):
