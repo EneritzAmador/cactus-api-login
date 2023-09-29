ccf33177dbc495484d62e15470a433310c1900e5
Ene AL <kaixo.eneal@gmail.com>
2023-09-29 16:29:57 +0200

    more files

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'uri_de_base_de_datos'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bc = Bcrypt(app)
